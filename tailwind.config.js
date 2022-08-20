6e0d5113b88fb3b2be8d34ca20aa6864edcbcd50
sanusiaz <sanusiabdulazeezadebayo1@gmail.com>
2022-08-20 08:18:07 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif'],
+        'outfit': ["'Outfit'", 'sans-serif']
+      }
+    }
+  },
+  plugins: [],
+}
